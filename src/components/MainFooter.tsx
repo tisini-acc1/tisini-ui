dfb0b5c8f19296cbfb0c4ddd6b175f071224f657
Felix Orinda <forinda82@gmail.com>
2023-07-07 08:00:12 +0300

    Setup tw and layout

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -6,12 +6,47 @@
  * @desc [Application footer]
  */
 
-"use client"
+"use client";
 
 function MainFooter() {
   return (
-    <div>MainFooter</div>
-  )
+    <div className="bg-primary-darker min-h-[20rem] p-4 py-8">
+      <footer className="flex flex-col justify-center items-center text-center text-white max-w-7xl mx-auto">
+        <div>
+          <h1 className="text-2xl font-bold">Felix Orinda</h1>
+        </div>
+        <div className="flex flex-row justify-center items-center space-x-4">
+          {/* Copyright */}
+
+          <div className="flex flex-row justify-center items-center space-x-4">
+            <a
+              href="
+              https://www.linkedin.com/in/felix-orinda-5a1b3b1b7/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 hover:text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <title>LinkedIn</title>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M14.828 14.828a4.5 4.5 0 01-6.364 0M16 10a6 6 0 10-12 0v6h6m-2 0h2m-2 0a4 4 0 118-4v4"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default MainFooter
+export default MainFooter;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          lightest: "#8AB6D6",
+          lighter: "#418FB9",
+          DEFAULT: "#05233D",
+          darker: "#031B29",
+          darkest: "#010E15",
+        },
+        secondary: {
+          lightest: "#FFE7AA",
+          lighter: "#FFD166",
+          DEFAULT: "#EF6F00",
+          darker: "#BF5400",
+          darkest: "#8F3F00",
+        },
+        light: {
+          DEFAULT: "#FDFDFD",
+          darker: "#F1F1F1",
+        },
+        black: {
+          lighter: "#1C1C1C",
+          DEFAULT: "#121212",
+        },
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
