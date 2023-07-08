9d26d7fa940d7b46136bb9c193b9681bccbcefd5
Felix Orinda <forinda82@gmail.com>
2023-07-08 20:14:07 +0300

    Add validation on login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,46 @@
 "use client";
 
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import TisiniValidator from "@/utils/tisini-validator";
+import { SignInUserInterface } from "@/types/types";
+
+const schema = yup
+  .object({
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters"),
+    // email: yup.string().email("Please provide valid email").required(),
+    phone_number: yup
+      .string()
+      .required("Phone number is required")
+      .min(10, "Phone number must be at least 10 characters")
+      .max(10, "Phone number must be at most 10 characters")
+      .matches(TisiniValidator.phoneRegex, {
+        message: "Phone number  format is invalid (0(7|1)xxxxxxxx)",
+        excludeEmptyString: true,
+      }),
+  })
+  .required();
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInUserInterface>({
+    resolver: yupResolver(schema),
+  });
   return (
     <div className="w-full">
       <div className="flex p-4 md:p-8 flex-col items-center justify-center max-w-7xl min-h-screen mx-auto">
         <form
           action=""
           className="flex flex-col gap-4 p-4 border border-gray-300 rounded-md shadow-md w-full md:max-w-[40rem]  "
+          onSubmit={handleSubmit((data) => console.log(data))}
         >
           <div className="flex items-center justify-center">
             {/* Image placeholder */}
@@ -23,21 +55,36 @@ export default function Login() {
             <label htmlFor="">Phone Number</label>
             <input
               type="text"
-              name="username"
-              id="username"
-              placeholder="Username"
-              className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-0 focus:ring-primary"
+              id="phone_number"
+              placeholder="Phone Number"
+              className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-0 focus:ring-primary
+              "
+              {...register("phone_number", { required: true })}
             />
+            {
+              errors.phone_number && (
+                <p className="text-red-500 text-sm">
+                  {errors.phone_number.message}
+                </p>
+              )
+            }
           </div>
           <div className="flex flex-col">
             <label htmlFor="">Password</label>
             <input
               type="password"
-              name="password"
               id="password"
               placeholder="Password"
               className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-0 focus:ring-primary"
+              {...register("password", { required: true })}
             />
+            {
+              errors.password && (
+                <p className="text-red-500 text-sm">
+                  {errors.password.message}
+                </p>
+              )
+            }
           </div>
 
           <div className="flex flex-col">
