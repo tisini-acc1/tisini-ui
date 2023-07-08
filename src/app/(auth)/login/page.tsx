55394898fa33f1616459f254cada7ef47b885b9c
Felix Orinda <forinda82@gmail.com>
2023-07-08 20:50:59 +0300

    Update login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,13 @@
 "use client";
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="w-full">
+      <div className="flex flex-col items-center justify-center max-w-7xl">
+       <form action="">
+          <input type="text" name="username" id="username" placeholder="Username" />
+          <input type="password" name="password" id="password" placeholder="Password" />
+       </form>
+      </div>
+    </div>
+  );
 }
