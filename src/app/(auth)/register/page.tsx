64b4e84ba8f2d88f39342307b8ba8ca2ade30140
Felix Orinda <forinda82@gmail.com>
2023-07-08 19:55:34 +0300

    Add form validation on register form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,71 +3,138 @@ import React from "react";
 import { SignupUserInterface } from "@/types/types";
 import "./register.css";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import TisiniValidator from "@/utils/tisini-validator";
 
+const schema = yup
+  .object({
+    first_name: yup.string().required("First name is required"),
+    last_name: yup.string().required("Last name is required"),
+    nickname: yup.string().required("Nickname is required"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters"),
+    email: yup.string().email("Please provide valid email").required(),
+    is_quiz_admin: yup.bool().default(false).optional(),
+    is_author: yup.bool().default(false).optional(),
+    phone_number: yup
+      .string()
+      .required()
+      .matches(TisiniValidator.phoneRegex, {
+        message: "Phone number  format is invalid (0(7|1)xxxxxxxx)",
+        excludeEmptyString: true,
+      })
+      .min(10, "Phone number must be at least 10 characters")
+      .max(10, "Phone number must be at most 10 characters"),
+  })
+  .required();
 export default function Register() {
-  const [formData, setFormData] = React.useState<SignupUserInterface>({
-    email: "",
-    first_name: "",
-    is_author: false,
-    is_quiz_admin: false,
-    last_name: "",
-    password: "",
-    phone_number: "",
-    nickname: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignupUserInterface>({
+    resolver: yupResolver(schema),
+    defaultValues: { is_quiz_admin: false, is_author: false },
   });
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFormData({ ...formData, [event.target.name]: event.target.value });
+  // };
   return (
     <main className="">
       <div className="flex flex-col items-center justify-center space-y-4 p-4 md:p-8 max-w-7xl min-h-screen mx-auto">
-        <form className="flex flex-col justify-center items-center space-y-4 md:max-w-[30rem] border py-4 rounded px-4 md:px-8">
+        <form
+          className="flex flex-col justify-center items-center space-y-4 md:max-w-[30rem] border py-4 rounded px-4 md:px-8"
+          onSubmit={handleSubmit((data) => console.log(data))}
+        >
           <h1 className="text-2xl font-bold text-center ">Register</h1>
           {/* First and last name row */}
           <div className="flex flex-col md:flex-row gap-2 justify-between w-full">
-            <input
-              className="register-input"
-              name="first_name"
-              placeholder="First Name"
-              type="text"
-              onChange={handleChange}
-            />
-            <input
-              className="register-input"
-              name="last_name"
-              placeholder="Last Name"
-              type="text"
-              onChange={handleChange}
-            />
+            <div className="w-full">
+              <label htmlFor="first_name">First Name</label>
+              <input
+                className="register-input"
+                placeholder="First Name"
+                type="text"
+                {...register("first_name", { required: true })}
+              />
+              {errors.first_name && (
+                <span className="text-red-500">
+                  {errors.first_name.message}
+                </span>
+              )}
+            </div>
+            <div className="w-full">
+              <label htmlFor="last_name">Last Name</label>
+              <input
+                className="register-input"
+                placeholder="Last Name"
+                type="text"
+                {...register("last_name", { required: true })}
+              />
+            </div>
           </div>
-          <input
-            className="register-input"
-            name="email"
-            placeholder="Email"
-            type="email"
-            onChange={handleChange}
-          />
-          <input
-            className="register-input"
-            name="nickname"
-            placeholder="Nickname"
-            type="text"
-            onChange={handleChange}
-          />{" "}
-          <input
-            className="register-input"
-            name="phone_number"
-            placeholder="Phone Number"
-            type="tel"
-            onChange={handleChange}
-          />
-          <input
-            className="register-input"
-            name="password"
-            placeholder="Password"
-            type="password"
-            onChange={handleChange}
-          />
+          <div className="w-full">
+            <label htmlFor="email">Email address</label>
+            <input
+              className="register-input"
+              placeholder="Email"
+              type="email"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <span className="text-red-500">{errors.email.message}</span>
+            )}
+          </div>
+          <div className="w-full">
+            <label htmlFor="phone_number">Nick name</label>
+            <input
+              className="register-input"
+              placeholder="Nickname"
+              type="text"
+              {...register("nickname", { required: true })}
+            />{" "}
+            {errors.nickname && (
+              <span className="text-red-500">{errors.nickname.message}</span>
+            )}
+          </div>
+          <div className="w-full">
+            <label htmlFor="phone_number">Phone number</label>
+            <input
+              className="register-input"
+              placeholder="Phone Number"
+              type="tel"
+              {...register("phone_number", {
+                required: true,
+                minLength: 10,
+                maxLength: 10,
+              })}
+            />
+            {errors.phone_number && (
+              <span className="text-red-500">
+                {errors.phone_number.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full">
+            <label htmlFor="password">Password</label>
+            <input
+              className="register-input"
+              placeholder="Password"
+              type="password"
+              {...register("password", {
+                required: true,
+                minLength: 6,
+                maxLength: 12,
+              })}
+            />
+            {errors.password && (
+              <span className="text-red-500">{errors.password.message}</span>
+            )}
+          </div>
           <button className="bg-primary text-white rounded py-2 px-4 w-full">
             Submit
           </button>
--- src/types/types.ts
+++ src/types/types.ts
@@ -3,10 +3,10 @@
 export interface SignupUserInterface {
   nickname: string; // Nickname with a max length of 255 and min length of 1
   email: string; // Email with a max length of 255 and min length of 1
-  first_name: string | null; // Optional first name with a max length of 255
-  last_name: string | null; // Optional last name with a max length of 255
+  first_name: string ; // Optional first name with a max length of 255
+  last_name: string ; // Optional last name with a max length of 255
   password: string; // Password with a max length of 68 and min length of 6
-  phone_number: string | null; // Optional phone number with a max length of 15
+  phone_number: string ; // Optional phone number with a max length of 15
   is_quiz_admin: boolean; // Is quiz admin flag
   is_author: boolean; // Is author flag
 }
