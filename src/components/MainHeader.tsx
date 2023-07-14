48e39cebce3e3691ce2dfe0fb6a3918ae0273d2d
Felix Orinda <forinda82@gmail.com>
2023-07-15 02:38:10 +0300

    Update available links on login and space elements

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -5,12 +5,11 @@
  * @modify date 2023-07-06 23:47:00
  * @desc [Application header]
  */
-"use client";
-
-import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import useAuth from "@/hooks/useAuth";
 
 const headerBottomList = [
   {
@@ -28,6 +27,7 @@ const headerBottomList = [
 ];
 
 export default function MainHeader() {
+  const { auth,dispatch } = useAuth();
   return (
     <header className="bg-primary w-full">
       <div className="max-w-7xl mx-auto p-4 w-full">
@@ -35,11 +35,14 @@ export default function MainHeader() {
           {/* Layer 1 */}
           <div className="w-full flex items-center justify-between">
             <div className="flex  items-center gap-2">
-              <FontAwesomeIcon
+              {/* <FontAwesomeIcon
                 icon={faBars}
                 className={`text-white hover:text-gray-400 font-bold w-6 h-6`}
-              />
+              /> */}
               <h1 className="text-white font-bold text-2xl">Tisini</h1>
+              {/* vertical line */}
+              <div className="w-0.5 h-6 bg-white mx-2"></div>
+              
               <div>
                 <nav>
                   <ul className="flex flex-row gap-4 items-center">
@@ -51,22 +54,7 @@ export default function MainHeader() {
                         Home
                       </Link>
                     </li>
-                    <li>
-                      <Link
-                        to="/"
-                        className="text-white hover:text-gray-400 font-bold"
-                      >
-                        Blogs
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/"
-                        className="text-white hover:text-gray-400 font-bold"
-                      >
-                        About
-                      </Link>
-                    </li>
+
                     <li>
                       <Link
                         to="/"
@@ -83,36 +71,48 @@ export default function MainHeader() {
                         Quiz
                       </Link>
                     </li>
-                    <li>
-                      <Link
-                        to="/"
-                        className="text-white hover:text-gray-400 font-bold"
-                      >
-                        Contact
-                      </Link>
-                    </li>
                   </ul>
                 </nav>
               </div>
             </div>
-            <div className="px-2 flex gap-2 items-center">
-              <Link
-                to="/login"
-                className="text-white hover:text-gray-400 font-bold"
-              >
-                Login
-              </Link>
-              <button className="bg-white text-primary rounded-md px-4 py-1">
-                Sign Up
-              </button>
-              <button>
-                <FontAwesomeIcon icon={faSearch} className="text-white" />
-              </button>
-            </div>
+            {auth.isAuthenticated ? (
+              <div className="flex items-center gap-2">
+                <Link
+                  to="/"
+                  className="text-white hover:text-gray-400 font-bold"
+                >
+                  {auth.user?.nickname}
+                </Link>
+
+                <button
+                  className="bg-red-300 text-primary rounded-md px-4"
+                  onClick={() => {
+                    dispatch({ type: "auth/LOGOUT" });
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            ) : (
+              <div className="px-2 flex gap-2 items-center">
+                <Link
+                  to="/auth/login"
+                  className="text-white hover:text-gray-400 font-bold"
+                >
+                  Login
+                </Link>
+                <button className="bg-white text-primary rounded-md px-4 py-1">
+                  Sign Up
+                </button>
+                <button>
+                  <FontAwesomeIcon icon={faSearch} className="text-white" />
+                </button>
+              </div>
+            )}
           </div>
           <hr className="bg-white" />
           {/* Layer 2 */}
-          <div className="flex gap-2">
+          <div className="flex gap-2 bg-primary-lighter">
             {headerBottomList.map((item) => (
               <div className="flex gap-2" key={item.title}>
                 <img src={item.img} alt="logo" width={20} height={20} />
