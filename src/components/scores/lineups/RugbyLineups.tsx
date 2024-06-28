bfa81e4b2535bcb9f91ad81cba404bb857bb9b12
McMaina <irutujmaina@gmail.com>
2024-06-28 13:07:43 +0300

    refactored partially rugby lineups tab

--- src/components/scores/lineups/RugbyLineups.tsx
+++ src/components/scores/lineups/RugbyLineups.tsx
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { Box, useTheme } from "@mui/material";
+import { useEffect, useState } from "react";
 
-import { tokens } from "@/theme/ScoresTheme";
 import { FixtureDetails, Lineup } from "@/lib/types/scores";
 import AwayPlayer from "@/components/scores/lineups/AwayPlayer";
 import HomePlayer from "@/components/scores/lineups/HomePlayer";
-import LineupsTitle from "@/components/scores/lineups/LineupsTitle";
 
 type LineUpsProps = {
   teams: [FixtureDetails];
@@ -13,9 +10,6 @@ type LineUpsProps = {
 };
 
 const RugbyLineups = ({ teams, squads }: LineUpsProps) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-
   const [homePlayers, setHomePlayers] = useState<Lineup[]>([]);
   const [awayPlayers, setAwayPlayers] = useState<Lineup[]>([]);
 
@@ -62,83 +56,65 @@ const RugbyLineups = ({ teams, squads }: LineUpsProps) => {
   }, [squads, teams]);
 
   return (
-    <Box bgcolor={colors.primary[300]}>
-      {/* first 11 section */}
-      <Box mb={2}>
-        <LineupsTitle title={"Starting players"} />
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          p={0.5}
-          mb={0.5}
-          bgcolor={colors.primary[400]}
-        >
-          <Box>
-            {homePlayers.map((player) => (
-              <Box key={player.player_id}>
-                {player.player_type === "first11" && (
-                  <Box>
-                    <HomePlayer name={player.pname} jersey={player.Jersey_No} />
-                  </Box>
-                )}
-              </Box>
-            ))}
-          </Box>
-          <Box>
-            {awayPlayers.map((player) => (
-              <Box key={player.player_id}>
-                {player.player_type === "first11" && (
-                  <Box>
-                    <AwayPlayer name={player.pname} jersey={player.Jersey_No} />
-                  </Box>
-                )}
-              </Box>
-            ))}
-          </Box>
-        </Box>
+    // first 11
+    <div className="flex flex-col space-y-2 p-2">
+      {/* header */}
+      <div className="flex justify-center items-center bg-gray-300 h-10 text-base md:text-2xl font-bold">
+        {"Starting Players"}
+      </div>
 
-        {/* subs section */}
-        <Box>
-          <LineupsTitle title={"Substitutes"} />
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            p={0.5}
-            mb={0.5}
-            bgcolor={colors.primary[400]}
-          >
-            <Box>
-              {homePlayers.map((player) => (
-                <Box key={player.player_id}>
-                  {player.player_type === "sub" && (
-                    <Box>
-                      <HomePlayer
-                        name={player.pname}
-                        jersey={player.Jersey_No}
-                      />
-                    </Box>
-                  )}
-                </Box>
-              ))}
-            </Box>
-            <Box>
-              {awayPlayers.map((player) => (
-                <Box key={player.player_id}>
-                  {player.player_type === "sub" && (
-                    <Box>
-                      <AwayPlayer
-                        name={player.pname}
-                        jersey={player.Jersey_No}
-                      />
-                    </Box>
-                  )}
-                </Box>
-              ))}
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
+      {/* home players */}
+      <div className="flex justify-between p-2">
+        <div>
+          {homePlayers.map((player) => (
+            <div key={player.player_id}>
+              {player.player_type === "first11" && (
+                <HomePlayer name={player.pname} jersey={player.Jersey_No} />
+              )}
+            </div>
+          ))}
+        </div>
+
+        {/* away players */}
+        <div>
+          {awayPlayers.map((player) => (
+            <div key={player.player_id}>
+              {player.player_type === "first11" && (
+                <AwayPlayer name={player.pname} jersey={player.Jersey_No} />
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* subs section */}
+      <div>
+        <div className="flex justify-center items-center bg-gray-300 h-10 text-base md:text-2xl font-bold">
+          {"Substitutes"}
+        </div>
+
+        <div className="flex justify-between p-2">
+          <div>
+            {homePlayers.map((player) => (
+              <div key={player.player_id}>
+                {player.player_type === "sub" && (
+                  <HomePlayer name={player.pname} jersey={player.Jersey_No} />
+                )}
+              </div>
+            ))}
+          </div>
+          <div>
+            {awayPlayers.map((player) => (
+              <div key={player.player_id}>
+                {player.player_type === "sub" && (
+                  <AwayPlayer name={player.pname} jersey={player.Jersey_No} />
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
