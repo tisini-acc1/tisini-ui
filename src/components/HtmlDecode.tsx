f0f1e865745acbdbb58038f97220924a9cfaea87
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-07-19 11:20:01 +0300

    Updated src/components/HtmlDecode.tsx file

--- src/components/HtmlDecode.tsx
+++ src/components/HtmlDecode.tsx
@@ -1,14 +1,64 @@
+import React from 'react';
 import he from 'he';
+import DOMPurify from 'dompurify';
 
-const HtmlDecoder = ({ html }: { html: string, exerpt?: boolean }) => {
-	return (
-	// Dangerously set innerHTML here.
-	// {
-	//     __html: exerpt ? he.decode(html).substring(0,200) : he.decode(html)
-	// }
-		<span dangerouslySetInnerHTML={{ __html: he.decode(html) }} />
+const REACT_PUBLIC_API_HOST = "https://system.tisini.co.ke";
 
-	);
+interface HtmlDecoderProps {
+  html: string;
+}
+
+const HtmlDecoder: React.FC<HtmlDecoderProps> = ({ html }) => {
+  const hostUrl = REACT_PUBLIC_API_HOST;
+
+  const UpdateImageUrls = (htmlString: string): string => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+
+    const imgs = doc.querySelectorAll('img');
+    imgs.forEach(img => {
+      const src = img.getAttribute('src');
+      if (src && (!src.startsWith('http') && !src.startsWith('https'))) {
+        img.setAttribute('src', `${hostUrl}${src}`);
+      }
+    });
+    return doc.body.innerHTML;
+  };
+
+  const decodedHtml = he.decode(html);
+  const sanitizedHtml = DOMPurify.sanitize(decodedHtml);
+  const updatedHtml = UpdateImageUrls(sanitizedHtml);
+
+  return (
+    <span
+      className='html-content'
+      dangerouslySetInnerHTML={{ __html: updatedHtml }}
+    />
+  );
 };
 
 export default HtmlDecoder;
+
+
+
+// import he from 'he';
+// import DOMPurify from 'dompurify';
+
+// const HtmlDecoder = ({ html }: { html: string, exerpt?: boolean }) => {
+// 	const hostUrl = "https://system.tisini.co.ke"
+
+// 	const UpdateImageUrls = (htmlString) => {
+// 		const parser = new DOMParser();
+// 		const 
+// 	}
+// 	return (
+// 	// Dangerously set innerHTML here.
+// 	// {
+// 	//     __html: exerpt ? he.decode(html).substring(0,200) : he.decode(html)
+// 	// }
+// 		<span dangerouslySetInnerHTML={{ __html: he.decode(html) }} />
+
+// 	);
+// };
+
+// export default HtmlDecoder;
