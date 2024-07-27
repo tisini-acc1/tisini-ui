a7e215b2997b140c88f1b3458af68ed72e769ff0
Felix Orinda <forinda82@gmail.com>
2024-07-27 13:04:04 +0300

    Update html decode

--- src/components/HtmlDecode.tsx
+++ src/components/HtmlDecode.tsx
@@ -1,59 +1,41 @@
+import React from 'react';
 import he from 'he';
-// import DOMPurify from 'dompurify';
+import DOMPurify from 'dompurify'
 
-const HtmlDecoder = ({ html }: { html: string, exerpt?: boolean }) => {
-	
-	return (
-	// Dangerously set innerHTML here.
-	// {
-	//     __html: exerpt ? he.decode(html).substring(0,200) : he.decode(html)
-	// }
-		<span dangerouslySetInnerHTML={{ __html: he.decode(html) }} />
+const REACT_PUBLIC_API_HOST = "https://system.tisini.co.ke";
 
-	);
+interface HtmlDecoderProps {
+  html: string;
+  exerpt?:boolean
+}
+
+const HtmlDecoder = ({ html,exerpt }:HtmlDecoderProps) => {
+  const hostUrl = REACT_PUBLIC_API_HOST;
+
+  const UpdateImageUrls = (htmlString: string): string => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+
+    const imgs = doc.querySelectorAll('img');
+    imgs.forEach(img => {
+      const src = img.getAttribute('src');
+      if (src && (!src.startsWith('http') && !src.startsWith('https'))) {
+        img.setAttribute('src', `${hostUrl}${src}`);
+      }
+    });
+    return doc.body.innerHTML;
+  };
+
+  const decodedHtml = he.decode(html);
+  const sanitizedHtml =  DOMPurify.sanitize(decodedHtml);
+  const updatedHtml = UpdateImageUrls(sanitizedHtml);
+
+  return (
+    <span
+      className='html-content'
+      dangerouslySetInnerHTML={{ __html: exerpt?updatedHtml.slice(0,200): updatedHtml }}
+    />
+  );
 };
 
 export default HtmlDecoder;
-
-// import React from 'react';
-// import he from 'he';
-
-// const REACT_PUBLIC_API_HOST = "https://system.tisini.co.ke";
-
-// interface HtmlDecoderProps {
-//   html: string;
-// }
-
-// const HtmlDecoder: React.FC<HtmlDecoderProps> = ({ html }) => {
-//   const hostUrl = REACT_PUBLIC_API_HOST;
-
-//   const UpdateImageUrls = (htmlString: string): string => {
-//     const parser = new DOMParser();
-//     const doc = parser.parseFromString(htmlString, 'text/html');
-
-//     const imgs = doc.querySelectorAll('img');
-//     imgs.forEach(img => {
-//       const src = img.getAttribute('src');
-//       if (src && (!src.startsWith('http') && !src.startsWith('https'))) {
-//         img.setAttribute('src', `${hostUrl}${src}`);
-//       }
-//     });
-//     return doc.body.innerHTML;
-//   };
-
-//   const decodedHtml = he.decode(html);
-//   const sanitizedHtml = DOMPurify.sanitize(decodedHtml);
-//   const updatedHtml = UpdateImageUrls(sanitizedHtml);
-
-//   return (
-//     <span
-//       className='html-content'
-//       dangerouslySetInnerHTML={{ __html: updatedHtml }}
-//     />
-//   );
-// };
-
-// export default HtmlDecoder;
-
-
-
