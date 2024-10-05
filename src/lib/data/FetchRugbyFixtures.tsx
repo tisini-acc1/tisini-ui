606e689e72fc932a3395596151a19f5708a28fdd
JI Maina <65528713+JI-Maina@users.noreply.github.com>
2024-10-05 15:38:15 +0300

    Update FetchRugbyFixtures.tsx

--- src/lib/data/FetchRugbyFixtures.tsx
+++ src/lib/data/FetchRugbyFixtures.tsx
@@ -3,7 +3,7 @@ import { Fixture } from "../types/scores";
 
 const FetchRugbyFixtures = async () => {
   const res = await axios.get<Fixture[]>(
-    "https://apis.tisini.co.ke/apiagent7.php?fixture=all&fixtype=rugby7"
+    "https://apis.tisini.co.ke/apiagent7.php?fixture=all&fixtype=rugby15"
   );
 
   return res.data;
