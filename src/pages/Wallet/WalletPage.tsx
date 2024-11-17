6d27628917c6ab229f0ca5504849563162208933
McMaina <irutujmaina@gmail.com>
2024-11-17 10:38:47 +0300

    Created withdrawal wallet functionality

--- src/pages/Wallet/WalletPage.tsx
+++ src/pages/Wallet/WalletPage.tsx
@@ -1,8 +1,14 @@
+import * as yup from "yup";
+import { AxiosError } from "axios";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ToastContainer, toast } from "react-toastify";
+
+import { cn } from "@/lib/cn";
+import { privateAxios } from "@/lib/api";
+import { useAppSelector } from "@/store/hooks";
 import MainFooter from "@/components/MainFooter";
 import MainHeader from "@/components/MainHeader";
-import { privateAxios } from "@/lib/api";
-import { cn } from "@/lib/cn";
-import { useAppSelector } from "@/store/hooks";
 import React, { Fragment, useState } from "react";
 import { Transaction } from "../Profile/ProfilePage";
 import {
@@ -21,6 +27,16 @@ type CreditScoreType = {
   recommendations: string[];
   last_updated: string;
 };
+
+const schema = yup
+  .object({
+    amount: yup
+      .string()
+      .required("Amount is required")
+      .min(1, "Amount must be at least KES 1"),
+  })
+  .required();
+
 export default function WalletPage() {
   const walletOpts = [
     "Account",
@@ -58,6 +74,42 @@ export default function WalletPage() {
     setUserTransactions(trans.data as Transaction[]);
   }, []);
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      const response = await (
+        await privateAxios.post("/users/withdraw_account/", {
+          withdraw_amount: data.amount,
+        })
+      ).data;
+
+      if (response.code === "1") {
+        toast.success(response.details);
+        reset({ amount: "" });
+      } else {
+        toast.error(response.details);
+      }
+
+      // console.log(response);
+    } catch (error: any) {
+      if (error instanceof AxiosError) {
+        // eslint-disable-next-line no-unsafe-optional-chaining, @typescript-eslint/no-unsafe-member-access
+        toast.error((error.response?.data).detail);
+      }
+    } finally {
+      // setIsLoading(false);
+      setIsWithdrawOpen(false);
+    }
+  });
+
   // const flatten =
   React.useEffect(() => {
     fetchBalance();
@@ -72,6 +124,7 @@ export default function WalletPage() {
       }}
     >
       <MainHeader />
+      <ToastContainer />
       <div className="w-full max-w-7xl mx-auto my-4 p-4">
         <div className="py-5 text-xl px-4 font-catamaran">
           <h1>
@@ -252,17 +305,44 @@ export default function WalletPage() {
                       </DialogTitle>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
-                          Your description goes here.
+                          Enter amount you wish to Withdraw.
                         </p>
                       </div>
 
-                      <div className="mt-4">
+                      <div className="flex flex-col">
+                        {/* <label htmlFor="">amount</label> */}
+                        <input
+                          type="number"
+                          id="amount"
+                          placeholder="Enter Amount"
+                          aria-invalid={errors.amount ? "true" : "false"}
+                          autoComplete="tel-national"
+                          className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-0 focus:ring-primary
+                  "
+                          {...register("amount", { required: true })}
+                        />
+                        {errors.amount && (
+                          <p className="text-red-500 text-sm">
+                            {errors.amount.message}
+                          </p>
+                        )}
+                      </div>
+
+                      <div className="mt-4 flex gap-4">
                         <button
                           type="button"
-                          className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                           onClick={() => setIsWithdrawOpen(false)}
                         >
                           Close
+                        </button>
+
+                        <button
+                          type="button"
+                          className="px-4 py-2 text-sm font-medium text-white bg-green-600 rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          onClick={() => onSubmit()}
+                        >
+                          Withdraw
                         </button>
                       </div>
                     </DialogPanel>
