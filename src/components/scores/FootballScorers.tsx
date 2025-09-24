8c8b9d0ff723c52e5bbf4b32ef3af65c970ab924
mcmaina <irutujmaina@gmail.com>
2025-09-24 13:14:16 +0300

    added football goal scorers

--- src/components/scores/FootballScorers.tsx
+++ src/components/scores/FootballScorers.tsx
@@ -1,57 +1,78 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import { BallScorer } from "@/lib/types/leagues";
+import React from "react";
 
-function createData(Player: string, Team: string, G: number, A: number) {
-  return { Player, Team, G, A };
-}
-
-const rows = [
-  createData("John Doe", "Manchester United", 24, 4),
-  createData("Mason Greenwood", "Manchester United", 23, 3),
-  createData("Jadon Sancho", "Machester United", 16, 6),
-  createData("Kevin Etemesi", "Nairobi City Stars", 10, 3),
-  createData("Benson Omalla", "Gor Mahia", 10, 3),
-  createData("John Doe", "Manchester United", 24, 4),
-  createData("Mason Greenwood", "Manchester United", 23, 3),
-  createData("Jadon Sancho", "Machester United", 16, 6),
-  createData("Kevin Etemesi", "Nairobi City Stars", 10, 3),
-  createData("Benson Omalla", "Gor Mahia", 10, 3),
-];
-
-export default function FootballScorers() {
+const FootballScorers = ({ players }: { players: BallScorer[] }) => {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Player</TableCell>
-            <TableCell align="center">Team</TableCell>
-            <TableCell align="right">G</TableCell>
-            <TableCell align="right">A</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.Player}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.Player}
-              </TableCell>
-              <TableCell align="center">{row.Team}</TableCell>
-              <TableCell align="right">{row.G}</TableCell>
-              <TableCell align="right">{row.A}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className="w-full p-4 max-w-6xl mx-auto">
+      <div className="rounded-xl border shadow-sm overflow-hidden">
+        <div className="overflow-x-auto">
+          <table className="w-full">
+            <thead className="bg-primary/30 font-bold">
+              <tr>
+                <th className="px-6 py-3 text-left text-xs bg-primary/30 text-primary uppercase tracking-wider">
+                  Player
+                </th>
+                {/* <th className="px-3 py-3 text-right text-xs bg-primary/30 text-primary uppercase tracking-wider">
+                  Tries
+                </th>
+                <th className="px-3 py-3 text-right text-xs bg-primary/30 text-primary uppercase tracking-wider">
+                  Conversions
+                </th>
+                <th className="hidden sm:table-cell px-3 py-3 text-right text-xs bg-primary/30 text-primary uppercase tracking-wider">
+                  Penalties
+                </th>
+                <th className="hidden sm:table-cell px-3 py-3 text-right text-xs bg-primary/30 text-primary uppercase tracking-wider">
+                  Drop Goals
+                </th> */}
+                <th className="px-6 py-3 text-right text-xs bg-primary/30 text-primary uppercase tracking-wider">
+                  Goals
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-muted">
+              {players?.map((player) => (
+                <tr
+                  key={player.playerid}
+                  className="hover:bg-primary/10 transition-colors"
+                >
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0 h-10 w-10 flex items-center justify-center rounded-full bg-primary text-primary-foreground font-medium">
+                        {player.playername[0]}
+                      </div>
+                      <div className="ml-4">
+                        <div className="text-sm font-medium text-foreground capitalize">
+                          {player.playername}
+                        </div>
+                        <div className="text-sm text-gray-500">
+                          {player.teamname}
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  {/* <td className="px-3 py-4 whitespace-nowrap text-center font-bold">
+                    {player.trys ?? "-"}
+                  </td>
+                  <td className="px-3 py-4 whitespace-nowrap text-center text-secondary-foreground font-bold">
+                    {player.conversion === 0 ? "-" : player.conversion}
+                  </td>
+                  <td className="hidden sm:table-cell px-3 py-4 whitespace-nowrap text-center font-bold">
+                    {player.penalty === 0 ? "-" : player.penalty}
+                  </td>
+                  <td className="hidden sm:table-cell px-3 py-4 whitespace-nowrap text-center font-bold">
+                    {player.dropgoal ?? "-"}
+                  </td> */}
+                  <td className="px-6 py-4 whitespace-nowrap text-center text-primary font-bold">
+                    {player.goal}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default FootballScorers;
